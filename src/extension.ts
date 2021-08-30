90685369752b83aced23ae07490801515f96abbb
safiir <809320935@qq.com>
2021-08-31 01:18:30 +0800

    adjust indentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,299 +1,318 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 const translate = require("./translate");
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "vampire" is now active!');
+  console.log('Congratulations, your extension "vampire" is now active!');
 
-	registerMap(context);
+  registerMap(context);
 
-	registerUniq(context);
+  registerUniq(context);
 
-	registerSort(context);
+  registerSort(context);
 
-	registerJoin(context);
+  registerJoin(context);
 
-	registerShuffle(context);
+  registerShuffle(context);
 
-	registerReverse(context);
+  registerReverse(context);
 
-	registerTranslate(context);
+  registerTranslate(context);
 
-	registerCapitalize(context);
+  registerCapitalize(context);
 }
 
 /**
  * register reverse extension
- * @param context 
+ * @param context
  */
 function registerReverse(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.reverse", () => {
+  let disposable = vscode.commands.registerCommand("extension.reverse", () => {
+    let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-		if (!editor) {
-			return;
-		}
+    let reversed = editor?.selections
+      .map((selection) => editor?.document.getText(selection))
+      .reverse();
 
-		let reversed = editor?.selections.map(selection => editor?.document.getText(selection)).reverse();
+    editor?.edit((builder) => {
+      editor?.selections.forEach((selection, index) => {
+        builder.replace(selection, "" + reversed[index]);
+      });
+    });
+  });
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, '' + reversed[index]);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
  * register join extension
- * @param context 
+ * @param context
  */
 function registerJoin(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.join", async () => {
+  let disposable = vscode.commands.registerCommand(
+    "extension.join",
+    async () => {
+      let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-		if (!editor) {
-			return;
-		}
+      const delimiter = await vscode.window.showInputBox();
 
-		const delimiter = await vscode.window.showInputBox();
+      let selections = editor?.selections.map((selection) =>
+        editor?.document.getText(selection)
+      );
 
-		let selections = editor?.selections.map(selection => editor?.document.getText(selection));
+      let joined = selections.join(delimiter);
 
-		let joined = selections.join(delimiter);
+      editor?.edit((builder) => {
+        editor?.selections.forEach((selection, index) => {
+          builder.replace(selection, index === 0 ? "" + joined : "");
+        });
+      });
+    }
+  );
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, index === 0 ? '' + joined : '');
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
  * register unique extension
- * @param context 
+ * @param context
  */
 function registerUniq(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.uniq", () => {
+  let disposable = vscode.commands.registerCommand("extension.uniq", () => {
+    let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-		if (!editor) {
-			return;
-		}
+    let selections = editor?.selections.map((selection) =>
+      editor?.document.getText(selection)
+    );
+    let uniques = Array.from(new Set(selections));
 
-		let selections = editor?.selections.map(selection => editor?.document.getText(selection));
-		let uniques = Array.from(new Set(selections));
+    editor?.edit((builder) => {
+      editor?.selections.forEach((selection, index) => {
+        builder.replace(selection, "" + (uniques[index] || ""));
+      });
+    });
+  });
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, '' + (uniques[index] || ''));
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
  * register capitalize extension
- * @param context 
+ * @param context
  */
 function registerCapitalize(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.capitalize", () => {
+  let disposable = vscode.commands.registerCommand(
+    "extension.capitalize",
+    () => {
+      let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+      editor?.selections.forEach(async (selection) => {
+        let content = editor?.document.getText(selection);
+        const result = content?.trim().split(/\s+/).map(capitalize).join(" ");
+        editor?.edit((builder) => {
+          builder.replace(selection, "" + result);
+        });
+      });
+    }
+  );
 
-		editor?.selections.forEach(async selection => {
-			let content = editor?.document.getText(selection);
-			const result = content?.trim().split(/\s+/).map(capitalize).join(" ");
-			editor?.edit(builder => {
-				builder.replace(selection, '' + result);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
  * register translate extension
- * @param context 
+ * @param context
  */
 function registerTranslate(context: vscode.ExtensionContext) {
-	let out: vscode.OutputChannel;
+  let out: vscode.OutputChannel;
 
-	let disposable = vscode.commands.registerCommand("extension.trans", async () => {
-		let target = "中文"; //await vscode.window.showQuickPick(Object.values(translate.Translate.Lang));
+  let disposable = vscode.commands.registerCommand(
+    "extension.trans",
+    async () => {
+      let target = "中文"; //await vscode.window.showQuickPick(Object.values(translate.Translate.Lang));
 
-		const editor = vscode.window.activeTextEditor;
+      const editor = vscode.window.activeTextEditor;
 
-		if (editor === undefined) {
-			return;
-		}
+      if (editor === undefined) {
+        return;
+      }
 
-		if (!out) {
-			out = vscode.window.createOutputChannel("翻译");
-			out.show();
-		}
+      if (!out) {
+        out = vscode.window.createOutputChannel("翻译");
+        out.show();
+      }
 
-		const worker = new translate.Translate(out);
+      const worker = new translate.Translate(out);
 
-		const words: any[] = unique(editor.selections.map(selection => editor.document.getText(selection)));
+      const words: any[] = unique(
+        editor.selections.map((selection) => editor.document.getText(selection))
+      );
 
-		let translations = await Promise.all(words.map(async word => {
-			return await worker.translate(word, target);
-		}));
+      let translations = await Promise.all(
+        words.map(async (word) => {
+          return await worker.translate(word, target);
+        })
+      );
 
-		const mapping = {};
+      const mapping: any = {};
 
-		words.forEach((word, index) => {
-			mapping[word] = translations[index];
-		});
+      words.forEach((word, index) => {
+        mapping[word] = translations[index];
+      });
 
-		editor.edit(builder => {
-			editor.selections.forEach((selection) => {
-				const word = editor.document.getText(selection);
-				const translation = mapping[word];
-				if (word) {
-					builder.replace(selection, '' + translation);
-				}
-			});
-		});
-	});
+      editor.edit((builder) => {
+        editor.selections.forEach((selection) => {
+          const word = editor.document.getText(selection);
+          const translation = mapping[word];
+          if (word) {
+            builder.replace(selection, "" + translation);
+          }
+        });
+      });
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function unique(xs: any[]) {
-	return Array.from(new Set(xs));
+  return Array.from(new Set(xs));
 }
 
 function registerSort(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.sort", () => {
+  let disposable = vscode.commands.registerCommand("extension.sort", () => {
+    let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-		if (!editor) {
-			return;
-		}
+    let sorted = editor?.selections
+      .map((selection) => editor?.document.getText(selection))
+      .sort();
 
-		let sorted = editor?.selections.map(selection => editor?.document.getText(selection)).sort();
+    editor?.edit((builder) => {
+      editor?.selections.forEach((selection, index) => {
+        builder.replace(selection, "" + sorted[index]);
+      });
+    });
+  });
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, '' + sorted[index]);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-
 function registerShuffle(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.shuffle", () => {
+  let disposable = vscode.commands.registerCommand("extension.shuffle", () => {
+    let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-		if (!editor) {
-			return;
-		}
+    let elements = editor?.selections.map((selection) =>
+      editor?.document.getText(selection)
+    );
+    let shuffled = shuffle(elements);
 
-		let elements = editor?.selections.map(selection => editor?.document.getText(selection));
-		let shuffled = shuffle(elements);
+    editor?.edit((builder) => {
+      editor?.selections.forEach((selection, index) => {
+        builder.replace(selection, "" + shuffled[index]);
+      });
+    });
+  });
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, '' + shuffled[index]);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function registerMap(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("extension.map", async () => {
+  let disposable = vscode.commands.registerCommand(
+    "extension.map",
+    async () => {
+      const transformer = await vscode.window.showInputBox();
 
-		const transformer = await vscode.window.showInputBox();
+      if (!transformer) {
+        return;
+      }
 
-		if (!transformer) {
-			return;
-		}
+      const functor = new Function("$", `return ${transformer}`);
 
-		const functor = new Function("$", `return ${transformer}`);
+      let editor = vscode.window.activeTextEditor;
 
-		let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-		if (!editor) {
-			return;
-		}
+      let elements: any;
 
-		let elements: any;
+      try {
+        elements = editor.selections
+          .map((selection) => editor?.document.getText(selection))
+          .map((original) => {
+            try {
+              return functor(original);
+            } catch (error) {
+              vscode.window.showErrorMessage(error.message);
+              throw new Error("transform error");
+            }
+          });
+      } catch (error) {
+        return;
+      }
 
-		try {
-			elements = editor.selections
-				.map(selection => editor?.document.getText(selection))
-				.map(original => {
-					try {
-						return functor(original);
-					} catch (error) {
-						vscode.window.showErrorMessage(error.message);
-						throw new Error("transform error");
-					}
-				});
-		} catch (error) {
-			return;
-		}
+      editor?.edit((builder) => {
+        editor?.selections.forEach((selection, index) => {
+          builder.replace(selection, "" + elements[index]);
+        });
+      });
+    }
+  );
 
-		editor?.edit(builder => {
-			editor?.selections.forEach((selection, index) => {
-				builder.replace(selection, '' + elements[index]);
-			});
-		});
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 /**
  * Fisher-Yates (aka Knuth) Shuffle Algorithm
- * @param array 
- * @returns 
+ * @param array
+ * @returns
  */
 function shuffle(array: any) {
-	var currentIndex = array.length, randomIndex;
+  var currentIndex = array.length,
+    randomIndex;
 
-	// While there remain elements to shuffle...
-	while (currentIndex !== 0) {
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
 
-		// And swap it with the current element.
-		[array[currentIndex], array[randomIndex]] = [
-			array[randomIndex], array[currentIndex]];
-	}
-
-	return array;
+  return array;
 }
-
 
 /**
  * capitalize a string
- * @param str 
- * @returns 
+ * @param str
+ * @returns
  */
 function capitalize(str: string) {
-	return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+  return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 export function deactivate() { }
--- src/translate.js
+++ src/translate.js
@@ -1,227 +1,229 @@
-'use strict';
+"use strict";
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true,
 });
 const http = require("http");
 const vscode = require("vscode");
 const crypto = require("crypto");
 const querystring = require("querystring");
-const colors = require('colors');
+const colors = require("colors");
 
-const {
-    table
-} = require('table');
+const { table } = require("table");
 
 class TextFilter {
-    static filter(q) {
-        q = q.replace(TextFilter.COMPILE, (e) => {
-            return ' ' + e;
-        });
-        q = q.replace(TextFilter.COMPILE_CHAR, ' ');
-        q = q.replace(TextFilter.COMPILE_SPAN, ' ');
-        return q;
-    }
+  static filter(q) {
+    q = q.replace(TextFilter.COMPILE, (e) => {
+      return " " + e;
+    });
+    q = q.replace(TextFilter.COMPILE_CHAR, " ");
+    q = q.replace(TextFilter.COMPILE_SPAN, " ");
+    return q;
+  }
 }
 
 TextFilter.COMPILE = /[A-Z]{1}[a-z]+/g;
 TextFilter.COMPILE_CHAR = /[-_\n\r\t*]/g;
 TextFilter.COMPILE_SPAN = /[\ ]+/g;
 class Translate {
-    constructor(out) {
-        this.appKey = '';
-        this.appSecret = '';
-        this.out = out;
-    }
-    md5(content) {
-        let md5 = crypto.createHash('md5');
-        md5.update(content);
-        let req = md5.digest('hex');
-        return req;
-    }
-    sign(a, s) {
-        let signStr = a.appKey + a.q + a.salt + s;
-        a.sign = this.md5(signStr).toUpperCase();
-    }
-    guid() {
-        let a = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-            let r = Math.random() * 16 | 0;
-            let v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-        return a;
-    }
+  constructor(out) {
+    this.appKey = "";
+    this.appSecret = "";
+    this.out = out;
+  }
+  md5(content) {
+    let md5 = crypto.createHash("md5");
+    md5.update(content);
+    let req = md5.digest("hex");
+    return req;
+  }
+  sign(a, s) {
+    let signStr = a.appKey + a.q + a.salt + s;
+    a.sign = this.md5(signStr).toUpperCase();
+  }
+  guid() {
+    let a = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+      let r = (Math.random() * 16) | 0;
+      let v = c === "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+    return a;
+  }
 
-    async fy(q, target) {
-        const request = {
-            'q': '',
-            'from': Translate.Lang.EN,
-            'to': target,
-            'appKey': '',
-            'salt': '',
-            'sign': '',
-        };
-        request.q = TextFilter.filter(q);
-        request.salt = this.guid();
-        request.appKey = this.appKey;
+  async fy(q, target) {
+    const request = {
+      q: "",
+      from: Translate.Lang.EN,
+      to: target,
+      appKey: "",
+      salt: "",
+      sign: "",
+    };
+    request.q = TextFilter.filter(q);
+    request.salt = this.guid();
+    request.appKey = this.appKey;
 
-        this.sign(request, this.appSecret);
-        let reqUrl = Translate.API_URL + querystring.stringify(request);
+    this.sign(request, this.appSecret);
+    let reqUrl = Translate.API_URL + querystring.stringify(request);
 
-        return await new Promise((resolve, reject) => {
-            http.get(reqUrl, (res) => {
-                const {
-                    statusCode
-                } = res;
-                const contentType = res.headers['content-type'];
-                let error;
-                if (statusCode !== 200) {
-                    error = new Error('请求失败。\n' +
-                        `状态码: ${statusCode}`);
-                } else if (contentType === undefined || !/^application\/json/.test(contentType)) {
-                    error = new Error('无效的 content-type.\n' +
-                        `期望 application/json 但获取的是 ${contentType}`);
-                }
-                if (error) {
-                    console.error(error.message);
-                    vscode.window.showInformationMessage(`错误: ${error.message}`);
-                    // 消耗响应数据以释放内存
-                    res.resume();
-                    return;
-                }
-                res.setEncoding('utf8');
-                let rawData = '';
-                res.on('data', (chunk) => {
-                    rawData += chunk;
-                });
-                res.on('end', () => {
-                    try {
-                        const parsedData = JSON.parse(rawData);
+    return await new Promise((resolve, reject) => {
+      http
+        .get(reqUrl, (res) => {
+          const { statusCode } = res;
+          const contentType = res.headers["content-type"];
+          let error;
+          if (statusCode !== 200) {
+            error = new Error("请求失败。\n" + `状态码: ${statusCode}`);
+          } else if (
+            contentType === undefined ||
+            !/^application\/json/.test(contentType)
+          ) {
+            error = new Error(
+              "无效的 content-type.\n" +
+                `期望 application/json 但获取的是 ${contentType}`
+            );
+          }
+          if (error) {
+            console.error(error.message);
+            vscode.window.showInformationMessage(`错误: ${error.message}`);
+            // 消耗响应数据以释放内存
+            res.resume();
+            return;
+          }
+          res.setEncoding("utf8");
+          let rawData = "";
+          res.on("data", (chunk) => {
+            rawData += chunk;
+          });
+          res.on("end", () => {
+            try {
+              const parsedData = JSON.parse(rawData);
 
-                        if (parsedData.errorCode !== '0') {
-                            let msg = Translate.ErrorCode[parsedData.errorCode];
-                            parsedData.errorMsg = msg !== undefined ? msg : '';
-                            vscode.window.showInformationMessage(parsedData.errorMsg);
-                        } else {
-                            let web = parsedData.web;
-                            let extra = web?.map(el => [el.key, el.value]);
+              if (parsedData.errorCode !== "0") {
+                let msg = Translate.ErrorCode[parsedData.errorCode];
+                parsedData.errorMsg = msg !== undefined ? msg : "";
+                vscode.window.showInformationMessage(parsedData.errorMsg);
+              } else {
+                let web = parsedData.web;
+                let extra = web?.map((el) => [el.key, el.value]);
 
-                            const config = {
-                                border: {
-                                    topBody: `─`,
-                                    topJoin: `┬`,
-                                    topLeft: `┌`,
-                                    topRight: `┐`,
+                const config = {
+                  border: {
+                    topBody: `─`,
+                    topJoin: `┬`,
+                    topLeft: `┌`,
+                    topRight: `┐`,
 
-                                    bottomBody: `─`,
-                                    bottomJoin: `┴`,
-                                    bottomLeft: `└`,
-                                    bottomRight: `┘`,
+                    bottomBody: `─`,
+                    bottomJoin: `┴`,
+                    bottomLeft: `└`,
+                    bottomRight: `┘`,
 
-                                    bodyLeft: `│`,
-                                    bodyRight: `│`,
-                                    bodyJoin: `│`,
+                    bodyLeft: `│`,
+                    bodyRight: `│`,
+                    bodyJoin: `│`,
 
-                                    joinBody: `─`,
-                                    joinLeft: `├`,
-                                    joinRight: `┤`,
-                                    joinJoin: `┼`
-                                }
-                            };
+                    joinBody: `─`,
+                    joinLeft: `├`,
+                    joinRight: `┤`,
+                    joinJoin: `┼`,
+                  },
+                };
 
-                            const innerConfig = {
-                                border: {
-                                    topBody: ``,
-                                    topJoin: ``,
-                                    topLeft: ``,
-                                    topRight: ``,
+                const innerConfig = {
+                  border: {
+                    topBody: ``,
+                    topJoin: ``,
+                    topLeft: ``,
+                    topRight: ``,
 
-                                    bottomBody: ``,
-                                    bottomJoin: ``,
-                                    bottomLeft: ``,
-                                    bottomRight: ``,
+                    bottomBody: ``,
+                    bottomJoin: ``,
+                    bottomLeft: ``,
+                    bottomRight: ``,
 
-                                    bodyLeft: ``,
-                                    bodyRight: ``,
-                                    bodyJoin: ``,
+                    bodyLeft: ``,
+                    bodyRight: ``,
+                    bodyJoin: ``,
 
-                                    joinBody: ``,
-                                    joinLeft: ``,
-                                    joinRight: ``,
-                                    joinJoin: ``
-                                }
-                            };
+                    joinBody: ``,
+                    joinLeft: ``,
+                    joinRight: ``,
+                    joinJoin: ``,
+                  },
+                };
 
-                            const data = [
-                                ['原文', request.q],
-                                ['结果', parsedData.translation],
-                                ['词义', extra ? table(extra, innerConfig) : null]
-                            ];
+                const data = [
+                  ["原文", request.q],
+                  ["结果", parsedData.translation],
+                  ["词义", extra ? table(extra, innerConfig) : null],
+                ];
 
-                            this.out.appendLine(table(data, config).green);
+                this.out.appendLine(table(data, config).green);
 
-                            resolve(parsedData.translation);
-                        }
-                    } catch (e) {
-                        console.error(e.message);
-                        vscode.window.showInformationMessage(e.message);
-                    }
-                });
-            }).on('error', (e) => {
-                console.error(`错误: ${e.message}`);
-                vscode.window.showInformationMessage(`错误: ${e.message}`);
-            });
-        });
-    }
-    async translate(content, target) {
-        let settings = vscode.workspace.getConfiguration();
-        //let settings = vscode.workspace.getConfiguration('cpplint');
-        this.appKey = settings.get('vampire.translate.youdao.appKey', '');
-        this.appSecret = settings.get('vampire.translate.youdao.appSecret', '');
-        if (this.appSecret && this.appKey) {
-            if (content) {
-                return await this.fy(content, target);
+                resolve(parsedData.translation);
+              }
+            } catch (e) {
+              console.error(e.message);
+              vscode.window.showInformationMessage(e.message);
             }
-        } else {
-            vscode.window.showErrorMessage("需要配置有道翻译 appKey 和 appSecret !");
-        }
+          });
+        })
+        .on("error", (e) => {
+          console.error(`错误: ${e.message}`);
+          vscode.window.showInformationMessage(`错误: ${e.message}`);
+        });
+    });
+  }
+  async translate(content, target) {
+    let settings = vscode.workspace.getConfiguration();
+    //let settings = vscode.workspace.getConfiguration('cpplint');
+    this.appKey = settings.get("vampire.translate.youdao.appKey", "");
+    this.appSecret = settings.get("vampire.translate.youdao.appSecret", "");
+    if (this.appSecret && this.appKey) {
+      if (content) {
+        return await this.fy(content, target);
+      }
+    } else {
+      vscode.window.showErrorMessage("需要配置有道翻译 appKey 和 appSecret !");
     }
+  }
 }
-Translate.API_URL = 'http://openapi.youdao.com/api?';
+Translate.API_URL = "http://openapi.youdao.com/api?";
 Translate.r1 = "";
 Translate.ErrorCode = {
-    '101': '缺少必填的参数',
-    '102': '不支持的语言类型',
-    '103': '翻译文本过长',
-    '104': '不支持的API类型',
-    '105': '不支持的签名类型',
-    '106': '不支持的响应类型',
-    '107': '不支持的传输加密类型',
-    '108': 'appKey无效',
-    '109': 'batchLog格式不正确',
-    '110': '无相关服务的有效实例',
-    '111': '开发者账号无效',
-    '113': 'q不能为空',
-    '201': '解密失败，可能为DES,BASE64,URLDecode的错误',
-    '202': '签名检验失败',
-    '203': '访问IP地址不在可访问IP列表',
-    '205': '请求的接口与应用的平台类型不一致',
-    '301': '辞典查询失败',
-    '302': '翻译查询失败',
-    '303': '服务端的其它异常',
-    '401': '账户已经欠费',
-    '411': '访问频率受限,请稍后访问',
-    '412': '长请求过于频繁，请稍后访问',
+  101: "缺少必填的参数",
+  102: "不支持的语言类型",
+  103: "翻译文本过长",
+  104: "不支持的API类型",
+  105: "不支持的签名类型",
+  106: "不支持的响应类型",
+  107: "不支持的传输加密类型",
+  108: "appKey无效",
+  109: "batchLog格式不正确",
+  110: "无相关服务的有效实例",
+  111: "开发者账号无效",
+  113: "q不能为空",
+  201: "解密失败，可能为DES,BASE64,URLDecode的错误",
+  202: "签名检验失败",
+  203: "访问IP地址不在可访问IP列表",
+  205: "请求的接口与应用的平台类型不一致",
+  301: "辞典查询失败",
+  302: "翻译查询失败",
+  303: "服务端的其它异常",
+  401: "账户已经欠费",
+  411: "访问频率受限,请稍后访问",
+  412: "长请求过于频繁，请稍后访问",
 };
 Translate.Lang = {
-    'zh-CHS': '中文',
-    'ja': '日文',
-    'EN': '英文',
-    'ko': '韩文',
-    'fr': '法文',
-    'ru': '俄文',
-    'pt': '葡萄牙文',
-    'es': '西班牙文',
-    'vi': '越南文',
+  "zh-CHS": "中文",
+  ja: "日文",
+  EN: "英文",
+  ko: "韩文",
+  fr: "法文",
+  ru: "俄文",
+  pt: "葡萄牙文",
+  es: "西班牙文",
+  vi: "越南文",
 };
 exports.Translate = Translate;
 //# sourceMappingURL=translate.js.map
